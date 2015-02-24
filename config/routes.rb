addbe2dbd52b6adc468033c5b5d9f72a30b513b2
Sara Gibbons <saragibby@gmail.com>
2015-02-24 11:10:20 -0500

    add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   root 'games#index'
 
   resources :games
+
+  namespace :api do
+    namespace :v1 do
+      resources :games
+      resources :rounds
+      resources :players
+    end
+  end
+
 end
